ccdc91fe5d34ce9f9cb831628dc919e44bc47f1c
ArcVaw <94964623+archievawser@users.noreply.github.com>
2025-09-27 02:36:44 +0930

    Initial commit

---
+++ Source/CustomShaders/Private/CustomShaders.cpp
@@ -0,0 +1,18 @@
+﻿#include "CustomShaders.h"
+
+#define LOCTEXT_NAMESPACE "FCustomShadersModule"
+
+void FCustomShadersModule::StartupModule()
+{
+    FString shaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders/"));
+    AddShaderSourceDirectoryMapping("/CustomShaders", shaderDirectory);
+}
+
+void FCustomShadersModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCustomShadersModule, CustomShaders)
---
+++ Source/Ocean.Target.cs
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class OceanTarget : TargetRules
+{
+	public OceanTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+
+		ExtraModuleNames.AddRange( new string[] { "Ocean" } );
+		RegisterModulesCreatedByRider();
+	}
+
+	private void RegisterModulesCreatedByRider()
+	{
+		ExtraModuleNames.AddRange(new string[] { "CustomShaders" });
+	}
+}
